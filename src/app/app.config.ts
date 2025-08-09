483ee9312f5a9dc1f2a218b07a8cce3632e63fa6
Mark Corzilius <marccorzprivate@gmail.com>
2025-08-09 20:11:36 +0200

    added listner for notes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
+// app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLD6t9_IXWRj0n81bmbSvZI-_Gr82gajY",
+  authDomain: "danotes-32a27.firebaseapp.com",
+  projectId: "danotes-32a27",
+  storageBucket: "danotes-32a27.firebasestorage.app",
+  messagingSenderId: "574570580876",
+  appId: "1:574570580876:web:8faceccc6f8c713944a4a9"
+};
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ]
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,19 +1,66 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
-
+import { Firestore, collectionData, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { Note } from './../interfaces/note.interface';
+ 
 @Injectable({
   providedIn: 'root'
 })
   
 export class NoteListService {
 
+
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubNotes;
+  unsubTrash;
+
+
+
   firestore: Firestore = inject(Firestore);
   itemCollection = collection(this.firestore, 'items');
+
   
-  item$;
+
   constructor() { 
-    this.item$ = collectionData(this.getNotesRef());
+
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+    
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id || "",
+      type: obj.type || "",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
+  
+  ngonDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    })
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    })
   }
 
   getNotesRef() {
